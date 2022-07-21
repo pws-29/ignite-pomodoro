dd4db96c91d04ec34374c87834c43e079b5bc54b
Pietro Weg <pietro.sera@compasso.com.br>
2022-07-21 14:31:55 -0300

    feat: page layout container

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,11 +1,25 @@
 import { Outlet } from "react-router-dom";
+import styled from "styled-components";
 import { Header } from "../components/Header";
 
 export function DefaultLayout() {
   return (
-    <div>
+    <LayoutContainer>
       <Header />
       <Outlet />
-    </div>
-  )
-}
+    </LayoutContainer>
+  );
+};
+
+export const LayoutContainer = styled.div`
+  max-width: 74rem;
+  height: calc(100vh - 10rem);
+  margin: 5rem auto;
+  padding: 2.5rem;
+
+  background-color: ${props => props.theme["gray-800"]};
+  border-radius: 8px;
+
+  display: flex;
+  flex-direction: column;
+`
