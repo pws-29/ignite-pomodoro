29b21f97c9c82ff954c66b1131ae2b5dce8d7424
Pietro Weg <pietro.sera@compasso.com.br>
2022-07-24 18:37:29 -0300

    file: files structure

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,24 +1,4 @@
-import { Scroll, Timer } from "phosphor-react"
-import { NavLink } from "react-router-dom"
 import styled from "styled-components"
-
-import logoIgnite from "../assets/logo-ignite.svg"
-
-export function Header() {
-  return (
-    <HeaderContainer>
-      <img src={logoIgnite} alt="" />
-      <nav>
-        <NavLink to="/" title="Timer">
-          <Timer size={24} />
-        </NavLink>
-        <NavLink to="/history" title="Histórico">
-          <Scroll size={24} />
-        </NavLink>
-      </nav>
-    </HeaderContainer>
-  )
-};
 
 export const HeaderContainer = styled.header`
   display: flex;
