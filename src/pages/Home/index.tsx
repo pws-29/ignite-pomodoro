c7c8a5009db0cef7187f418363d05a96d4f389c3
Pietro Weg <pietro.sera@compasso.com.br>
2022-07-27 12:03:55 -0300

    feat: initial logic for countdown

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -49,6 +49,7 @@ export function Home() {
 
   const [cycles, setCycles] = useState<Cycle[]>([]);
   const [activeCycleId, setActiveCycleId] = useState<string | null>(null);
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0);
 
   const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
@@ -74,7 +75,15 @@ export function Home() {
   }
 
   const activeCycle = cycles.find(cycle => cycle.id === activeCycleId);
-  console.log(activeCycle);
+
+  const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0;
+  const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0;
+
+  const minutesAmount = Math.floor(currentSeconds / 60)
+  const secondsAmount = currentSeconds % 60;
+
+  const minutes = String(minutesAmount).padStart(2, '0');
+  const seconds = String(secondsAmount).padStart(2, '0');
 
 
   // Transforma o input task em um campo controlado. Renderiza sempre que alterado
@@ -112,11 +121,11 @@ export function Home() {
         </FormContainer>
 
         <CountdownContainer>
-          <span>0</span>
-          <span>0</span>
+          <span>{minutes[0]}</span>
+          <span>{minutes[1]}</span>
           <Separator>:</Separator>
-          <span>0</span>
-          <span>0</span>
+          <span>{seconds[0]}</span>
+          <span>{seconds[1]}</span>
         </CountdownContainer>
 
         <StartCountButton disabled={isSubmitDisabled} type="submit">
