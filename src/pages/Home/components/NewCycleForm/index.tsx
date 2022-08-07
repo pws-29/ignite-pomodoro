21b1c693edbb1f698ae476113cfcf02a685d6365
Pietro Weg <pietro.sera@compasso.com.br>
2022-08-07 19:09:31 -0300

    styles: movendo estilos para seus componentes

--- src/pages/Home/components/NewCycleForm/index.tsx
+++ src/pages/Home/components/NewCycleForm/index.tsx
@@ -5,30 +5,32 @@ import {
 } from "./styles"
 
 export function NewCycleForm() {
-  <FormContainer>
-    <label htmlFor="task">Vou trabalhar em</label>
-    <TaskInput
-      id="task"
-      placeholder="Dê um nome para o seu projeto"
-      list="task-suggestions"
-      {...register('task')}
-    />
-    <datalist id="task-suggestions">
-      <option value="Projeto 1"></option>
-      <option value="Projeto 2"></option>
-      <option value="Projeto 3"></option>
-    </datalist>
+  return (
+    <FormContainer>
+      <label htmlFor="task">Vou trabalhar em</label>
+      <TaskInput
+        id="task"
+        placeholder="Dê um nome para o seu projeto"
+        list="task-suggestions"
+        {...register('task')}
+      />
+      <datalist id="task-suggestions">
+        <option value="Projeto 1"></option>
+        <option value="Projeto 2"></option>
+        <option value="Projeto 3"></option>
+      </datalist>
 
-    <label htmlFor="minutesAmout">durante</label>
-    <MinutesAmoutInput
-      id="minutesAmount"
-      type="number"
-      placeholder="00"
-      step={5}
-      min={5}
-      max={60}
-      {...register('minutesAmount', { valueAsNumber: true })}
-    />
-    <span>minutos.</span>
-  </FormContainer>
+      <label htmlFor="minutesAmout">durante</label>
+      <MinutesAmoutInput
+        id="minutesAmount"
+        type="number"
+        placeholder="00"
+        step={5}
+        min={5}
+        max={60}
+        {...register('minutesAmount', { valueAsNumber: true })}
+      />
+      <span>minutos.</span>
+    </FormContainer>
+  )
 }
