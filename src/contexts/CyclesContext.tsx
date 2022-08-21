b7147042f4fc0d48ab7b393d6979196300ff8c18
Pietro Weg <pietro.sera@compasso.com.br>
2022-08-21 13:32:10 -0300

    separando as actions

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -1,6 +1,7 @@
 import { createContext, useState, useReducer } from "react";
-import { ActionTypes, Cycle, cyclesReducer } from '../reducers/cycles'
+import { Cycle, cyclesReducer } from '../reducers/cycles/reducer'
 import { v4 as uuidv4 } from 'uuid';
+import { addNewCycleAction, interruptCurrentCycleAction, markCurrentCycleAsFinishedAction } from "../reducers/cycles/actions";
 
 interface CreateCycleData {
   task: string;
@@ -39,12 +40,7 @@ export function CyclesContextProvider({ children }: CyclesContextProviderProps) 
   }
 
   function markCurrentCycleAsFinished() {
-    dispatch({
-      type: ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED,
-      payload: {
-        activeCycleId
-      }
-    })
+    dispatch(markCurrentCycleAsFinishedAction())
 
     // setCycles(prevState =>
     //   prevState.map(cycle => {
@@ -68,12 +64,7 @@ export function CyclesContextProvider({ children }: CyclesContextProviderProps) 
       startDate: new Date(),
     }
 
-    dispatch({
-      type: ActionTypes.ADD_NEW_CYCLE,
-      payload: {
-        newCycle,
-      }
-    })
+    dispatch(addNewCycleAction(newCycle));
 
     // setCycles(prevState => [...prevState, newCycle])
     // setActiveCycleId(id);
@@ -82,12 +73,7 @@ export function CyclesContextProvider({ children }: CyclesContextProviderProps) 
   }
 
   function interruptCurrentCycle() {
-    dispatch({
-      type: ActionTypes.INTERRUPT_CURRENT_CYCLE,
-      payload: {
-        activeCycleId
-      }
-    })
+    dispatch(interruptCurrentCycleAction())
 
     // TODO: entender fluxo 
     // setCycles(prevState =>
